47e8210a27f06061ca53c37a7c1f713eaf20da28
hoaian03IT <dangtranhoaian8603@gmail.com>
2024-12-14 18:27:06 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: import.meta.env.VITE_PORT || 5138,
+    },
+});
