cd10f79c70e08e6d4c8e677f2586827c832ade6b
hoaian03IT <dangtranhoaian8603@gmail.com>
2024-12-14 18:30:48 +0700

    change port

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: import.meta.env.VITE_PORT || 5138,
+        port: 5138,
     },
 });
